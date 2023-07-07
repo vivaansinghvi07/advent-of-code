a1773bcbb4d57613378d2e2bfeb24247ac81186b
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-07-07 12:45:42 -0400

    solved both in one line

---
+++ 02_rock_paper_scissors/step_1.py
@@ -0,0 +1,1 @@
+print(sum([0, 3, 6][[o!=3, o!=1, o!=2][s-1] + [o==2, o==3, o==1][s-1]] + o for s, o in map(lambda x: map(lambda c: ['A', 'B', 'C', 'X', 'Y', 'Z'].index(c) % 3 + 1, x.split()), open("input.txt"))))
---
+++ 02_rock_paper_scissors/step_2.py
@@ -0,0 +1,1 @@
+print(sum([0, 3, 6][o-1] + [1, 2, 3][(s+o) % 3] for s, o in map(lambda x: map(lambda c: ['A', 'B', 'C', 'X', 'Y', 'Z'].index(c) % 3 + 1, x.split()), open("input.txt"))))
