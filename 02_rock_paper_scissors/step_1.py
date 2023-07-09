d7ae2756b75c2b2db91fb6c2df53d976440ec03e
Vivaan Singhvi <s945958@student.knoxschools.org>
2023-07-08 23:42:25 -0400

    fix

--- 02_rock_paper_scissors/step_1.py
+++ 02_rock_paper_scissors/step_1.py
@@ -1,1 +1,1 @@
-print(sum([0, 3, 6][o-s+1] + o for s, o in map(lambda x: map(lambda c: ['A', 'B', 'C', 'X', 'Y', 'Z'].index(c) % 3 + 1, x.split()), open("input.txt"))))
+print(sum([0, 3, 6][(o-s+1)%3] + o for s, o in map(lambda x: map(lambda c: ['A', 'B', 'C', 'X', 'Y', 'Z'].index(c) % 3 + 1, x.split()), open("input.txt"))))
