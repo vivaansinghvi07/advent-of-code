e68c811b2c259291f4f19102f73aa80b6767cb6c
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-07-10 10:04:58 -0400

    ashwefuhsdfiuh

--- count_lines.py
+++ count_lines.py
@@ -1,7 +1,5 @@
 import os
 dirs = sorted([d for d in os.listdir(".") if d[:2].isnumeric()], key=lambda x: int(x[:2]))
 files = [[f for f in os.listdir(f"./{d}") if f != "input.txt"] for d in dirs]
-for (d, fs) in zip(dirs, files):
-    print(f"{d}:")
-    for f in fs:
-        print(f"\t{''.join(f.split('.')[:-1])}: {len([*open(f'{d}/{f}')])}")
+n, t = '\n', '\t'
+print('\n'.join([f"{d}:{n}{f'{n}'.join([t+''.join(f.split('.')[:-1]) + ':' + str(len([*open(f'{d}/{f}')])) for f in fs])}" for d, fs in zip(dirs, files)]))
