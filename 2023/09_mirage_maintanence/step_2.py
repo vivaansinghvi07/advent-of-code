c200dc976b3df61aab24f7a5dda0560cf896f83d
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2023-12-10 18:47:08 -0500

    made the solution, 2 lines for each file

---
+++ 2023/09_mirage_maintanence/step_2.py
@@ -0,0 +1,2 @@
+def handle_diffs(l: list[int]) -> int: return 0 if not any(l) else l[0] - handle_diffs([j-i for i,j in zip(l, l[1:])])
+print(sum([handle_diffs(l) for l in map(lambda x: list(map(int, x.split())), open("input.txt"))]))
